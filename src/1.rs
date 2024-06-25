b60003b540d6ede24e616ef3e7477b4123308695
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-06-25 20:08:32 +0530

    basic

---
+++ src/1.rs
@@ -0,0 +1,28 @@
+use std::future::Future;
+use std::ptr::null;
+use std::task::{ Context, Poll, RawWaker, RawWakerVTable, Waker };
+
+fn wake(_data: *const ()) {}
+fn noop(_data: *const ()) {}
+
+static VTABLE: RawWakerVTable = RawWakerVTable::new(
+    |data| RawWaker::new(data, &VTABLE),
+    wake,
+    wake,
+    noop
+);
+
+pub fn main() {
+    let task = async { 13 };
+
+    let waker = RawWaker::new(null(), &VTABLE);
+    let waker = unsafe { Waker::from_raw(waker) };
+
+    let mut cx = Context::from_waker(&waker);
+    let mut task = Box::pin(task);
+
+    match task.as_mut().poll(&mut cx) {
+        Poll::Ready(value) => println!("ready {value:? }"),
+        Poll::Pending => println!("pending"),
+    }
+}
