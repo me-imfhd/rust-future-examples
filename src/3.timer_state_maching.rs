1e40dcbc69b156d49f590bbd31f099599b0a0eb4
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-06-26 16:33:24 +0530

    next making a brain

--- src/3.timer_state_maching.rs
+++ src/3.timer_state_maching.rs
@@ -151,3 +151,41 @@ pub fn new_executor_and_spawner() -> (Executor, Spawner) {
     let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TASKS);
     (Executor { ready_queue }, Spawner { task_sender })
 }
+
+fn main() {
+    let (executor, spawner) = new_executor_and_spawner();
+
+    // Spawn a task to print before and after waiting on a timer.
+    spawner.spawn(async {
+        println!("howdy1!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("done1!");
+    });
+    spawner.spawn(async {
+        println!("howdy2!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("done2!");
+    });
+    spawner.spawn(async {
+        println!("howdy3!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(3, 0)).await;
+        println!("done3!");
+    });
+    spawner.spawn(async {
+        println!("howdy4!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(4, 0)).await;
+        println!("done4!");
+    });
+
+    // Drop the spawner so that our executor knows it is finished and won't
+    // receive more incoming tasks to run.
+    drop(spawner);
+
+    // Run the executor until the task queue is empty.
+    // This will print "howdy!", pause, and then print "done!".
+    executor.run();
+}
